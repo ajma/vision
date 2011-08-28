5813ffc75b97fc6190c746a90e1c95cffdfe5b5a
Andrew Ma <ajma@live.com>
2011-08-27 21:12:53 -0700

    Adding history pages

---
+++ Controllers/HistoryController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Vision.Controllers
+{
+    public class HistoryController : Controller
+    {
+        VisionContext context = new VisionContext();
+
+        public ActionResult History()
+        {
+            var history = context.GlassesHistory.OrderByDescending(h => h.RemovalDate).ToArray();
+            return View(history);
+        }
+
+    }
+}
