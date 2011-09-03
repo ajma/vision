ea398e000788e8a409593927282f6df53d389e38
Andrew Ma <ajma@live.com>
2011-09-03 00:03:34 -0700

    Fixing calculation of group when adding new glasses

--- Controllers/AddController.cs
+++ Controllers/AddController.cs
@@ -19,11 +19,19 @@ namespace Vision.Controllers
         [HttpPost]
         public ActionResult Add(Glasses glasses)
         {
-            // figure out which group this pair goes to
-            glasses.Group = (int)Math.Floor(glasses.OD_Spherical);
+            // figure out which group this pair goes to. the algorith for this is rounding the OD (right) Sph value away from 0
+            bool positive = (glasses.OD_Spherical >= 0);
+            glasses.Group = (int)Math.Ceiling(Math.Abs(glasses.OD_Spherical));
+            // anything over 10 gets grouped into the 20 group
+            if (glasses.Group > 10)
+                glasses.Group = 20;
+            if (!positive)
+                glasses.Group *= -1;
+
             // grab all glasses in this group so that I can look for the next pair of glasses
             var numbers = context.Glasses.Where(g => g.Group == glasses.Group).Select(g => g.Number).OrderBy(g => g).ToArray();
             glasses.Number = numbers.Length > 0 ? numbers.Max() + 1 : 1;
+
             // try to look for a gap. If there is a gap in the number, fill it in.
             for (int i = 0; i < numbers.Length; i++)
             {
@@ -34,7 +42,7 @@ namespace Vision.Controllers
                 }
             }
 
-            // add date time stamp
+            // add UTC date time stamp
             glasses.InsertDate = DateTime.UtcNow;
 
             context.Glasses.Add(glasses);
