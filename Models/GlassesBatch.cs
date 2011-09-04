c772045a1f24d0faa9175e72b8d27b6509ab74b1
ajma <ajma@live.com>
2011-09-04 12:36:27 -0700

    Need to make GlassesBatch.CallNumbers allow for longer than 128 length strings

--- Models/GlassesBatch.cs
+++ Models/GlassesBatch.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace Vision.Models
 {
     public class GlassesBatch
     {
         public int GlassesBatchID { get; set; }
+
+        [StringLength(2000)]
         public string CallNumbers { get; set; }
     }
 }
