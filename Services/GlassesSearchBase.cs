c1d391ceab34c6e5e13f9e8102271e0937020d59
Andrew Ma <ajma@live.com>
2011-10-01 22:53:43 -0700

    Refactoring search into an abstract class so that we can later have multiple search options

---
+++ Services/GlassesSearchBase.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Vision.Models;
+using System.Text;
+
+namespace Vision.Services
+{
+    public abstract class GlassesSearchBase
+    {
+        public GlassesSearchResult ScoreGlasses(Glasses search, Glasses glasses)
+        {
+            GlassesSearchResult result = new GlassesSearchResult { Glasses = glasses };
+            result.MatchScore = 100f;
+            StringBuilder details = new StringBuilder();
+
+            result.MatchScore -= getSphericalScore(search, glasses, details);
+            result.MatchScore -= getCylinderScore(search, glasses, details);
+            result.MatchScore -= getAxisScore(search, glasses, details);
+            result.MatchScore -= getAddScore(search, glasses, details);
+
+            result.MatchScore -= getSunglassesScore(search, glasses, details);
+            result.MatchScore -= getGenderScore(search, glasses, details);
+            result.MatchScore -= getSizeScore(search, glasses, details);
+
+            if (result.MatchScore < 0f)
+                result.MatchScore = 0f;
+
+            result.MatchScoreDetails = details.ToString();
+
+            return result;
+        }
+
+        protected abstract float getSphericalScore(Glasses search, Glasses glasses, StringBuilder details);
+        protected abstract float getCylinderScore(Glasses search, Glasses glasses, StringBuilder details);
+        protected abstract float getAxisScore(Glasses search, Glasses glasses, StringBuilder details);
+        protected abstract float getAddScore(Glasses search, Glasses glasses, StringBuilder details);
+
+        protected abstract float getSunglassesScore(Glasses search, Glasses glasses, StringBuilder details);
+        protected abstract float getGenderScore(Glasses search, Glasses glasses, StringBuilder details);
+        protected abstract float getSizeScore(Glasses search, Glasses glasses, StringBuilder details);
+
+        #region Helpers
+        /// <summary>
+        /// Figures out the number of quarter steps difference.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static int quarterStepsDifference(float a, float b)
+        {
+            // if there's one is + and the other is -, then we should never match
+            if ((a > 0 && b < 0) || (a < 0 && b > 0))
+                return -100;
+
+            if (b > 0)
+            {
+                return (int)((b - a) / 0.25f);
+            }
+            else
+            {
+                return (int)((a - b) / 0.25f);
+            }
+        }
+        #endregion
+    }
+}
---
+++ Services/MikeTamSearch.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vision.Services
+{
+    public class MikeTamSearch : GlassesSearchBase
+    {
+        protected override float getSphericalScore(Models.Glasses search, Models.Glasses glasses, System.Text.StringBuilder details)
+        {
+            float od_sph_score = 2 * scoreSpherical(search.OD_Spherical, glasses.OD_Spherical);
+            details.AppendFormat("OD Sph: -{0}\n", od_sph_score);
+
+            float os_sph_score = 2 * scoreSpherical(search.OS_Spherical, glasses.OS_Spherical);
+            details.AppendFormat("OS Sph: -{0}\n", os_sph_score);
+
+            // if the two eyes have an adjustment difference of more than 1 step, then penalize
+            float extraPenalty = 0f;
+            if (Math.Abs(od_sph_score - os_sph_score) > 2)
+            {
+                extraPenalty += 5;
+                details.Append("OS OD sph diff > 1 step: -5\n");
+            }
+
+            return od_sph_score + os_sph_score + extraPenalty;
+        }
+
+        protected override float getCylinderScore(Models.Glasses search, Models.Glasses glasses, System.Text.StringBuilder details)
+        {
+            float od_cyl_score = 2 * scoreCylindrical(search.OD_Cylindrical, glasses.OD_Cylindrical);
+            details.AppendFormat("OD Cyl: -{0}\n", od_cyl_score);
+
+            float os_cyl_score = 2 * scoreCylindrical(search.OS_Cylindrical, glasses.OS_Cylindrical);
+            details.AppendFormat("OS Cyl: -{0}\n", os_cyl_score);
+
+            // if the two eyes have an adjustment difference of more than 1 step, then penalize
+            float extraPenalty = 0f;
+            if (Math.Abs(od_cyl_score - os_cyl_score) > 2)
+            {
+                extraPenalty = 5f;
+                details.Append("OS OD cyl diff > 1 step: -5\n");
+            }
+
+            return od_cyl_score + os_cyl_score + extraPenalty;
+        }
+
+        protected override float getAxisScore(Models.Glasses search, Models.Glasses glasses, System.Text.StringBuilder details)
+        {
+            float od_axis_score = scoreAxis(search.OD_Cylindrical, search.OD_Axis, glasses.OD_Axis);
+            details.AppendFormat("OD Axis: -{0}\n", od_axis_score);
+
+            float os_axis_score = scoreAxis(search.OS_Cylindrical, search.OS_Axis, glasses.OS_Axis);
+            details.AppendFormat("OS Axis: -{0}\n", os_axis_score);
+
+            return od_axis_score + os_axis_score;
+        }
+
+        protected override float getAddScore(Models.Glasses search, Models.Glasses glasses, System.Text.StringBuilder details)
+        {
+            return 0f;
+        }
+
+        protected override float getSunglassesScore(Models.Glasses search, Models.Glasses glasses, System.Text.StringBuilder details)
+        {
+            return 0f;
+        }
+
+        protected override float getGenderScore(Models.Glasses search, Models.Glasses glasses, System.Text.StringBuilder details)
+        {
+            return 0f;
+        }
+
+        protected override float getSizeScore(Models.Glasses search, Models.Glasses glasses, System.Text.StringBuilder details)
+        {
+            return 0f;
+        }
+
+        private static float scoreSpherical(float search, float glasses)
+        {
+            if (search >= 0 && search >= glasses && glasses >= 0)
+            {
+                return (search - glasses) / 0.25f;
+            }
+            else if (search < 0 && search <= glasses && glasses <= 0)
+            {
+                return (search - glasses) / -0.25f;
+            }
+            return 100f;
+        }
+
+        private static float scoreCylindrical(float search, float glasses)
+        {
+            if (search - 0.25f <= glasses)
+            {
+                return Math.Abs(search - glasses) / 0.25f;
+            }
+            return 100f;
+        }
+
+        private static float scoreAxis(float search_cyl, int search_axis, int glasses_axis)
+        {
+            int reasonableRange = 5;
+            if (search_cyl >= -1.0f)
+                reasonableRange = 20;
+            else if (search_cyl >= -2.0f)
+                reasonableRange = 15;
+            else if (search_cyl >= -3.0f)
+                reasonableRange = 10;
+
+            int distance = Math.Abs(search_axis - glasses_axis);
+            distance = Math.Min(distance, 180 - distance);
+
+            if (distance < reasonableRange)
+                return (8 * distance) / reasonableRange;
+            else if (distance < reasonableRange * 2)
+                return 15;
+            else if (distance < reasonableRange * 3)
+                return 25;
+            else
+                return 35;
+        }
+    }
+}
