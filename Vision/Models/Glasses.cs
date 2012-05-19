46c8329784517860c4e2a4c5cdf252b900e2a21e
Andrew Ma <ajma@live.com>
2012-05-19 11:38:11 -0700

    Deleting all old code and creating a fresh template for the next version in the vnext branch

---
+++ Vision/Models/Glasses.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vision.Models
+{
+    public class Glasses
+    {
+        public int GlassesId { get; set; }
+        public int Group { get; set; }
+        public int Number { get; set; }
+        public Eye OD { get; set; }
+        public Eye OS { get; set; }
+        public bool Sunglasses { get; set; }
+        public char Size { get; set; }
+        public char Gender { get; set; }
+
+        public class Eye
+        {
+            public float Spherical { get; set; }
+            public float Cylindrical { get; set; }
+            public int Axis { get; set; }
+            public float Add { get; set; }
+        }
+    }
+}
