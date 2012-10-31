8a50aafc94becfadc750784ce30bc14aafa74512
Andrew Ma <ajma@live.com>
2012-10-31 15:22:45 -0700

    Setting up service to run web server. Shows notification when running

--- Server/android/Vision/src/com/andrewma/vision/MainActivity.java
+++ Server/android/Vision/src/com/andrewma/vision/MainActivity.java
@@ -1,20 +1,39 @@
 package com.andrewma.vision;
 
+import com.andrewma.vision.webserver.WebServerService;
+
+import android.view.View;
 import android.os.Bundle;
+import android.widget.Button;
 import android.app.Activity;
-import android.view.Menu;
+import android.content.Intent;
 
 public class MainActivity extends Activity {
 
+	private boolean mServerStarted = false;
+	
+	private Button mStartStopServerButton;
+	private Intent mWebServerService;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+        mWebServerService = new Intent(this, WebServerService.class);
+        
         setContentView(R.layout.activity_main);
+        
+        mStartStopServerButton = (Button)findViewById(R.id.startStopServerButton);
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
+    
+    public void startStopServerButtonClick(View v) {
+    	if(!mServerStarted) {
+    		startService(mWebServerService);
+    	} else {
+    		stopService(mWebServerService);
+    	}
+    	
+    	mServerStarted = !mServerStarted;
+    	mStartStopServerButton.setText(mServerStarted ? R.string.stopServer : R.string.startServer);
     }
 }
