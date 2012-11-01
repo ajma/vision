56202a4d9376cf66ac11e12a7a0e00de78cf6567
Andrew Ma <ajma@live.com>
2012-10-31 22:41:16 -0700

    Show wifi ip address on main activity

--- Server/android/Vision/src/com/andrewma/vision/MainActivity.java
+++ Server/android/Vision/src/com/andrewma/vision/MainActivity.java
@@ -1,15 +1,18 @@
 package com.andrewma.vision;
 
+import com.andrewma.vision.utils.NetworkUtils;
 import com.andrewma.vision.webserver.WebServerService;
 
 import android.view.View;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 import android.app.Activity;
 import android.content.Intent;
 
 public class MainActivity extends Activity {
 
+	private TextView mServerIpAddress;
 	private Button mStartStopServerButton;
 	private Intent mWebServerService;
 	
@@ -21,7 +24,10 @@ public class MainActivity extends Activity {
         
         setContentView(R.layout.activity_main);
         
+        mServerIpAddress = (TextView)findViewById(R.id.serverIpAddress);
         mStartStopServerButton = (Button)findViewById(R.id.startStopServerButton);
+        
+        mServerIpAddress.setText(NetworkUtils.getWifiIpAddress(this));
         mStartStopServerButton.setText(WebServerService.isWebServerRunning() ? R.string.stopServer : R.string.startServer);
     }
     
