a42c73363af7c2a3fc6c3f8cea0bc77d586dae6c
Andrew Ma <ajma@live.com>
2012-11-01 15:22:37 -0700

    Hiding action bar in WebViewActivity

--- Server/android/Vision/src/com/andrewma/vision/activity/WebViewActivity.java
+++ Server/android/Vision/src/com/andrewma/vision/activity/WebViewActivity.java
@@ -4,9 +4,12 @@ import com.andrewma.vision.R;
 import com.andrewma.vision.webserver.WebServerService;
 
 import android.os.Bundle;
+import android.view.Window;
 import android.webkit.WebView;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 
+@SuppressLint("SetJavaScriptEnabled")
 public class WebViewActivity extends Activity {
 
 	private WebView webview;
@@ -15,6 +18,7 @@ public class WebViewActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_webview);
 		
 		webview = (WebView)findViewById(R.id.webview);
