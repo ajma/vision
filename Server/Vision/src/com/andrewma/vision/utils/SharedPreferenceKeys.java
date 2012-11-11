d5aa65350e217455b67e17da9a918abed8a1dad6
Andrew Ma <ajma@live.com>
2012-11-10 17:23:36 -0800

    Allow web server to be auto-started when application launches

---
+++ Server/Vision/src/com/andrewma/vision/utils/SharedPreferenceKeys.java
@@ -0,0 +1,7 @@
+package com.andrewma.vision.utils;
+
+public class SharedPreferenceKeys {
+	public static final String SHARED_PREFERENCES_NAME = "com.andrewma.vision";
+	
+	public static final String SERVER_AUTOSTART = "SERVER_AUTOSTART";  
+}
