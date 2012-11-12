328f8a969033b3a88e9357a59bddb8bbfb50ad7c
Andrew Ma <ajma@live.com>
2012-11-11 21:27:51 -0800

    Expanding Add view to show new batch/load batch/help

--- Server/Vision/assets/js/views/AddView.js
+++ Server/Vision/assets/js/views/AddView.js
@@ -8,6 +8,20 @@ function($, _, Backbone, addTemplate, rxForm) {
 		render : function() {
 			this.$el.append(addTemplate).hide().fadeIn();
 			$('#rxform').append(rxForm);
+			
+			var hideFeatures = function() {
+				$('#add_features').slideUp();
+			};
+			
+			$('#newBatchFeatureIcon').click(function() {
+				$('#addGlassesForm').slideDown();
+				hideFeatures();
+			});
+			
+			$('#loadBatchFeatureIcon').click(function() {
+				$('#loadBatchModal').modal();
+			});
+			
 			$('#addButton').click(function() {
 				var test = $('#rxform').serialize();
 				$.post('/api/glasses/add', test, function() {
