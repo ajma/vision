431bba774a6a368e894697df18fb91a0fcca4f11
Andrew Ma <ajma@live.com>
2013-01-10 12:33:49 -0800

    Putting logic to figure out what the call number is when glasses are added

--- Server/Vision/src/com/andrewma/vision/webserver/controllers/GlassesController.java
+++ Server/Vision/src/com/andrewma/vision/webserver/controllers/GlassesController.java
@@ -1,6 +1,10 @@
 package com.andrewma.vision.webserver.controllers;
 
+import java.util.List;
+
 import android.content.Context;
+import android.util.FloatMath;
+import android.util.SparseBooleanArray;
 
 import com.andrewma.vision.database.DatabaseHelper;
 import com.andrewma.vision.models.Glasses;
@@ -17,7 +21,7 @@ public class GlassesController extends Controller {
 		super();
 		dbHelper = DatabaseHelper.getInstance(context);
 	}
-	
+
 	@Action
 	public Result Search(Glasses query) {
 		return GetAll();
@@ -37,7 +41,28 @@ public class GlassesController extends Controller {
 
 	@Action
 	public Result Add(Glasses glasses) {
+		// figure out which group this pair goes to. the algorithm for this is
+		// rounding the OD (right) Sph value away from 0
+		boolean positive = (glasses.OD_Spherical >= 0);
+		glasses.Group = (int) FloatMath.ceil(Math.abs(glasses.OD_Spherical));
+		// anything over 10 gets grouped into the 20 group
+		if (glasses.Group > 10)
+			glasses.Group = 20;
+		if (!positive)
+			glasses.Group *= -1;
+
+		// find number
+		final List<Glasses> glassesInGroup = dbHelper.executeSql(Glasses.class,
+				"SELECT * FROM Glasses WHERE [Group] = " + glasses.Group);
+		int max = 0;
+		for(Glasses g : glassesInGroup) {
+			if(g.Number > max)
+				max = g.Number;
+		}
+		if(max == glassesInGroup.size())
+			glasses.Number = max + 1;
+
 		dbHelper.insert(glasses);
-		return Result(NanoHTTPD.HTTP_OK, VisionHTTPD.MIME_JSON, true);
+		return Result(NanoHTTPD.HTTP_OK, VisionHTTPD.MIME_JSON, glasses);
 	}
 }
