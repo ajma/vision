996eef65f01a6fb04c201b8adc250191dfa8e2ff
Andrew Ma <ajma@live.com>
2013-01-10 12:39:08 -0800

    Adding classes for Batches

---
+++ Server/Vision/src/com/andrewma/vision/models/Batch.java
@@ -0,0 +1,29 @@
+package com.andrewma.vision.models;
+
+import com.andrewma.vision.database.core.DbDataType;
+import com.andrewma.vision.database.core.annotations.Column;
+import com.andrewma.vision.database.core.annotations.PrimaryKey;
+
+/**
+ * When glasses are added to the inventory in batches, this is used to keep
+ * track of which glasses were added into which batch
+ * 
+ * @author ajma
+ * 
+ */
+public class Batch {
+	@PrimaryKey
+	public int BatchId;
+
+	/**
+	 * Name of person working on this batch
+	 */
+	@Column(dataType = DbDataType.TEXT)
+	public String Name;
+
+	/**
+	 * Comma-delimited list of {@link Glasses#GlassesId} that are in this batch
+	 */
+	@Column(dataType = DbDataType.TEXT)
+	public String Glasses;
+}
