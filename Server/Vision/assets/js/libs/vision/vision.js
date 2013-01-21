1a0e2cc149d81eeae0c646e8dad30a5d3f558a56
Andrew Ma <ajma@live.com>
2013-01-20 20:11:48 -0800

    Creating a help js library to show help content. Starting with rxform keyboard shortcuts

--- Server/Vision/assets/js/libs/vision/vision.js
+++ Server/Vision/assets/js/libs/vision/vision.js
@@ -1,5 +1,13 @@
-define([ 'jquery', 'underscore', 'backbone', 'text!templates/home.html'],
-function($, _, Backbone, template) {
+define([ 'jquery', 'underscore', 'backbone', 'text!templates/help.html'],
+function($, _, Backbone, helpTemplate) {
+	var help = $(helpTemplate);
+	var showHelp = function(id) {
+		return function() {
+			$('#helpModal .modal-body').append(help.find('#' + id));
+			$('#helpModal').modal();
+		};
+	};
+
 	return {
 		post : function(url, data, success) {
 			$.post(url, data, function(data) {
@@ -13,5 +21,8 @@ function($, _, Backbone, template) {
 				success(data);
 			}).error(function() { $('#errorModal').modal(); });
 		},
+		help : {
+			rxFormShorcuts : showHelp('rxFormShorcuts')
+		}
 	};
 });
